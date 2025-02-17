f7496d5b41cf0302d86a5365577544fb5362ff1c
Peterson <petersondejesus99@gmail.com>
2025-02-17 08:59:12 -0300

    Alter text button in checkout

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -104,7 +104,7 @@
                         </div>
                     </div>
 
-                    <button type="submit" class="mt-4 w-full bg-blue-600 dark:bg-blue-400 text-white font-bold py-2 px-4 rounded-lg shadow hover:bg-blue-700 dark:hover:bg-blue-500">Finalizar Compra</button>
+                    <button type="submit" class="mt-4 w-full bg-blue-600 dark:bg-blue-400 text-white font-bold py-2 px-4 rounded-lg shadow hover:bg-blue-700 dark:hover:bg-blue-500">Finalizar Pagamento</button>
                 </form>
             </div>
 
